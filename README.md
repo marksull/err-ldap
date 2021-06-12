dac1b25959b50caf3063586802c4d89a4a8c84ab
marksull <mark@sullivans.id.au>
2021-06-12 21:23:47 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,77 @@
+err-backend-cisco-webex-teams
+======
+
+This is an err (http://errbot.io) backend for Cisco Webex Teams (https://www.webex.com/products/teams/index.html) 
+(formally Cisco Spark).
+
+This repo is based off my previous Cisco Spark repo (https://github.com/marksull/err-backend-cisco-spark) but with one 
+big difference: this uses websockets and not webhooks to communicate with Webex Teams. This makes a big difference when 
+it comes to deploying your bot behind a firewall or NAT device as the connection is initiated outbound and held open so
+there are no need for webhooks, firewall/nat changes, or tunnels (like NGROK) to provide access to your bot.
+
+The websocket implementation comes from: https://github.com/cgascoig/ciscospark-websocket
+
+## Status
+
+This backend is currently under development.
+
+
+## Installation
+
+```
+git clone https://github.com/marksull/err-backend-cisco-webex-teams.git
+```
+
+To your errbot config.py file add the following:
+
+```
+BACKEND = 'CiscoWebexTeams'
+BOT_EXTRA_BACKEND_DIR = '/path_to/err-backend-cisco-webex-teams'
+```
+
+## Bot Configuration
+
+
+To configure the bot you will need a Bot TOKEN. If you don't already have a bot setup on Cisco Webex Teams  details can
+be found here: https://developer.webex.com/bots.html.
+
+```python
+BOT_IDENTITY = {
+    'TOKEN': '<insert your token in here>',
+}
+```
+
+In a Webex Teams GROUP room (more than two people), to direct a command to the bot you need to prefix it with the name of the
+bot as you would any other person in the room (for example, type @ and select the bot name). 
+As Webex Teams will prefix the command with this name it is important that it is stripped from the 
+incoming command for it to be processed correctly. To achieve this add your bot name 
+(exactly as configured in Webex Teams) to the BOT_PREFIX:
+
+```python
+BOT_PREFIX = 'my-webex-teams-bot-name '
+```
+
+In a Webex Teams DIRECT room chat with the bot the bot prefix is not sent with the command. Ensure
+to enable BOT_PREFIX_OPTIONAL_ON_CHAT so that the prefix is not required for direct communication:
+
+```python
+BOT_PREFIX_OPTIONAL_ON_CHAT = True
+```
+
+## Cards
+
+A custom card callback handler has now been implemented to make it easier to work with cards. Refer to the
+example plugin [err-example-card](examples/err-example-cards)
+
+## Uploads
+
+While Webex Teams does not support the creation of a Message with both text and file(s) for upload, this backend will now automatically split the message and the file upload into multiple messages. Refer to the example  [err-example-upload](examples/err-example-upload)
+
+## Credit
+
+I unrestrainedly plagiarized from most of the already existing err backends and cgascoig's ciscospark-websocket implementation 
+(https://github.com/cgascoig/ciscospark-websocket).
+
+## Contributing
+
+Happy to accept Pull Requests
---
+++ errldap.py
@@ -0,0 +1,150 @@
+import ldap
+import functools
+
+
+def user_email(msg):
+    """
+    Use the email from the msg as the user search criteria
+
+    Args:
+        msg (errbot.backends.base.Message): The message from the bot that triggered the LDAP lookup
+
+    Returns:
+        str: The user ID that will be used to search LDAP
+
+    Raises:
+        ValueError: When user from email address can not be determined
+    """
+
+    # This should cater for Webex backend that contains a list of emails for user
+    if hasattr(msg.frm, "emails"):
+        email = (
+            msg.frm.emails[0] if isinstance(msg.frm.emails, list) else msg.frm.emails
+        )
+        return email.split("@")[0]
+
+    # This should cater for other backends like slack
+    if hasattr(msg.frm, "email"):
+        return msg.frm.email.split("@")[0]
+
+    # And just in case..don't want to return an empty user that could potentially match
+    raise ValueError("Cannot determine user ID from email address")
+
+
+def user_id(msg):
+    """
+    Use the userid from the msg as the user search criteria
+
+    Args:
+        msg (errbot.backends.base.Message): The message from the bot that triggered the LDAP lookup
+
+    Returns:
+        str: The user ID that will be used to search LDAP
+    """
+    if hasattr(msg.frm, "userid"):
+        return msg.frm.userid
+
+    raise ValueError("Cannot determine user ID from msg.userid")
+
+
+def connect(bot):
+    """
+    Initialize the LDAP connection
+
+    Override this method if you want a custom initialisation
+
+    Args:
+        bot (Errbot): The errbot instance
+
+    Returns:
+        ldap.functions.LDAPObject: An initialised LDAP Object
+    """
+    con = ldap.initialize(bot.bot_config.LDAP_URL)
+    con.simple_bind_s(bot.bot_config.LDAP_USERNAME, bot.bot_config.LDAP_PASSWORD)
+    return con
+
+
+def is_member(connection, bot, user, group):
+    """
+    Determine if the username provided is a member of the group
+
+    Override this method if you want to handle your search in a specific way
+
+    Args:
+        connection: (ldap.functions.LDAPObject): The session object for the LDAP connection
+        bot (Errbot): The errbot instance
+        user (str): The user that is to be search for in LDAP
+        group (str): The LDAP search base
+
+    Returns:
+        bool: True if the user is a member, else False
+    """
+    data = connection.search_s(
+        bot.bot_config.LDAP_SEARCH_BASE, ldap.SCOPE_SUBTREE, f"(CN={group})",
+    )
+
+    # Could not find the group
+    if not data:
+        return False
+
+    return user in [
+        m.decode("utf-8").split(",")[0].split("=")[1] for m in data[0][1]["member"]
+    ]
+
+
+def fail_message(user, msg, groups):
+    """
+    The message to be displayed to the user when they fail the LDAP validation
+
+    Override this method if you want your own custom message
+
+    Args:
+        user (str): The user ID that was used for the LDAP search
+        msg (errbot.backends.base.Message): The message from the bot that triggered the LDAP lookup
+        groups (list): The list og group(s) in which the user was searched
+
+    Returns:
+        str: The formatted failure message
+
+    """
+    return f"Sorry, ```{user}``` is not permitted to execute the command ```{msg}``` as you are not a member of ```{','.join(groups)}```"
+
+
+def ldap_verify(group, determine_user=user_email):
+    """"
+    Decorator to limit commands to a specific LDAP group or groups. The username will be derived from the person whom
+    issued the webex teams chat.
+
+    Notes for using this decorator:
+        - Any method using this decorator MUST use YIELD in lieu of RETURN for all bot directed output
+        - The decorator must be the outermost decorator (on top of all other decorators) to ensure the ldap
+          check is performed prior to the bot command being issued
+        - If more than one group is to be validated, then a list of groups should be provided. The first
+          occurrence of membership for any of the groups, the user will be considered verified
+
+    Args:
+        group (str|list): The LDAP group(s) to which this command should be restricted
+        determine_user: The method that will determine the username that will be used for the LDAP search
+    """
+
+    def decorator(func):
+        @functools.wraps(func)
+        def func_wrapper(self, msg, *args, **kwargs):
+
+            connection = connect(self)
+            user = determine_user(msg)
+            groups = group if isinstance(group, list) else [group]
+
+            for search_group in groups:
+                if is_member(connection, self, user, search_group):
+                    break
+            else:
+                yield fail_message(user=user, msg=msg, groups=groups)
+                return
+
+            for response in func(self, msg, *args, **kwargs):
+                yield response
+
+        return func_wrapper
+
+    return decorator
